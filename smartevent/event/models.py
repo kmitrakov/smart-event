92615995fc8201856a532b28783eff4d88f4ddc9
Kirill Mitrakov <k.mitrakov@gmail.com>
2022-11-20 18:50:56 +0300

    Model "event" added.

--- smartevent/event/models.py
+++ smartevent/event/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    status = models.IntegerField(default=0)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
